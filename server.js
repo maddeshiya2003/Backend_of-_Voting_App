050ead6f95970a8d08104f705fb0079960370f26
Nitesh Kumar Maddeshiya <maddeshiya2003@gmail.com>
2024-04-06 00:54:11 +0530

    correct candidate route to candidates route

--- server.js
+++ server.js
@@ -7,13 +7,20 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
+app.get("/", async (req,res) => {
+    
+    return res.status(200).send(
+        "Welcome to Voting app, here you can follow this Readme file for routing the ends points: <b>https://github.com/maddeshiya2003/Backend_of-_Voting_App/blob/main/README.md</b>");
+    
+  });
+
 // routes for user
 const userRoute = require("./routes/userRoutes");
 app.use("/user", userRoute)
 
 // routes for candidate
 const candidateRoute = require("./routes/candidateRoutes");
-app.use("/candidate", candidateRoute); //role must be admin
+app.use("/candidates", candidateRoute); //role must be admin
 
 app.listen(PORT, ()=>{
     console.log('listening on port 3000');
